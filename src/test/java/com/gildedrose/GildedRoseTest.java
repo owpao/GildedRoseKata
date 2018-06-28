1cbc355bd93c39e712f168b54c0b59573294e8fe
John Paolo Flores <john.paolo.flores@oocl.com>
2018-06-28 14:07:02 +0800

    Added Test Case for Backstage passes to a TAFKAL80ETC concert

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -24,4 +24,21 @@ public class GildedRoseTest {
         Assert.assertEquals(0,item.quality);
     }
 
+    @Test
+    public void reset_quality_of_item_to_zero_if_it_is_a_ticket_to_a_concert() throws Exception {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 4);
+        GildedRose gildedRose = new GildedRose(new VeryImportantObject(null)){
+            @Override
+            protected void InitializeItems() {
+                items = null;
+            }
+
+            @Override
+            protected void LogMessageDebug(String message) {
+            }
+        };
+        gildedRose.updateItem(item);
+        Assert.assertEquals(0,item.quality);
+    }
+
 }
