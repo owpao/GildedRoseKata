7c0b5a7db65cc57f8356481a8c0d6424fa006dfb
John Paolo Flores <john.paolo.flores@oocl.com>
2018-06-28 14:21:40 +0800

    Added Test Case for Aged Brie Item

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -25,6 +25,23 @@ public class GildedRoseTest {
     }
 
     @Test
+    public void given_item_is_aged_brie_and_quality_and_sell_in_is_zero_when_update_item_should_increment_quality_by_2() throws Exception {
+        Item item = new Item("Aged Brie", 0, 0);
+        GildedRose gildedRose = new GildedRose(new VeryImportantObject(null)){
+            @Override
+            protected void InitializeItems() {
+                items = null;
+            }
+
+            @Override
+            protected void LogMessageDebug(String message) {
+            }
+        };
+        gildedRose.updateItem(item);
+        Assert.assertEquals(2,item.quality);
+    }
+
+    @Test
     public void reset_quality_of_item_to_zero_if_it_is_a_ticket_to_a_concert() throws Exception {
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 4);
         GildedRose gildedRose = new GildedRose(new VeryImportantObject(null)){
@@ -40,5 +57,7 @@ public class GildedRoseTest {
         gildedRose.updateItem(item);
         Assert.assertEquals(0,item.quality);
     }
+
+    
 
 }
